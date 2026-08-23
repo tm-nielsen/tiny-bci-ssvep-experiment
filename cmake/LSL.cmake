1290871cbbbadc938d55326c9dae61a034207f77
Twig <git@twig.gay>
2026-08-23 13:32:17 -0600

    Added LSL config file to build process - extracted runtime output path expressions from copy assets build file for reuse - added custom target to copy LSL api config file from project root to output directory - added LSL config file to project root - excluded `lslver` target from `ALL` build

--- cmake/LSL.cmake
+++ cmake/LSL.cmake
@@ -24,3 +24,16 @@ FetchContent_Declare(liblsl
     GIT_TAG        v1.16.2
 )
 FetchContent_MakeAvailable(liblsl)
+
+set_target_properties(lslver PROPERTIES EXCLUDE_FROM_ALL ON)
+
+
+include(cmake/RuntimeOutputPath.cmake)
+set(CONFIG_FILE_PATH_EXPRESSION "${FULL_RUNTIME_OUTPUT_PATH_EXPRESSION}/lsl_api.cfg")
+
+add_custom_target(copy_lsl_configuration_file ALL
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    "${CMAKE_SOURCE_DIR}/lsl_api.cfg"
+    "${CONFIG_FILE_PATH_EXPRESSION}"
+  COMMENT "Copying lsl configuration file to output folder: ${CONFIG_FILE_PATH_EXPRESSION}"
+)
