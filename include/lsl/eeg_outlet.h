b697159659573e2dd07bf8044e149126b11a9fa3
Twig <git@twig.gay>
2026-09-01 14:52:23 -0600

    Simplified name of EEG outlet

--- include/lsl/eeg_outlet.h
+++ include/lsl/eeg_outlet.h
@@ -2,7 +2,7 @@
 # ifndef LSL_EEG_PIPE
 # define LSL_EEG_PIPE
 
-# define LSL_EEG_PIPE_STREAM_NAME "Tiny_BCI_Experiment_EEG_Redirect"
+# define LSL_EEG_PIPE_STREAM_NAME "Tiny_BCI_EEG"
 # define LSL_EEG_PIPE_STREAM_TYPE "EEG"
 # define LSL_EEG_PIPE_SOURCE_ID "tiny_bci_ssvep_experiment"
 
